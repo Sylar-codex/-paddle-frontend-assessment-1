4a1940e48b6e7d02e204084216a2593230614106
Sylar <user@Sylar-MacBook-Pro.local>
2022-07-18 00:22:59 +0800

    fixed bugs

--- README2.md
+++ README2.md
@@ -1,3 +1,12 @@
 ## Getting started with github 30
 
-    This project was built using React and its
+    This project was built using React and its hooks, also used axios to fetch api from github endpoint
+    useEffect was a hook used as the lifecycle method for integrating the api. also CSS and its animations were used to style the application. also made responsive using media query. Also implemented an infinite scroll.
+
+# usuage of application
+
+Whenever the page loads it displays data from the api which shows descending order of most starred respository in the last 30 days. users can easily scroll inifinitely of different results
+
+# imporovements
+
+if this was not an assessment, I would have the users to be able to connect with the repository and its owner on github and also view a snippet of the repository
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,100 +1,85 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import moment from "moment";
 
 function Display() {
   const [val, setVal] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [inComplete, setInComplete] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const observe = useRef();
-  useEffect(() => {
+  let pageNumber = 1;
+  const fetchData = () => {
     Axios.get(
       `https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc&page=${pageNumber}
 
     `
-    ).then((res) => {
-      //console.log(res.data.incomplete_results);
-      setInComplete(res.data.incomplete_results);
-      console.log(inComplete);
-      console.log(pageNumber);
-      setVal(res.data.items);
-      setLoading(false);
-    });
-  }, [pageNumber, inComplete]);
+    )
+      .then((res) => {
+        const newArr = [];
+        console.log("yes", newArr);
+        res.data.items.forEach((p) => newArr.push(p));
+        setVal((arr) => [...arr, ...newArr]);
+        console.log("no", val);
+        setLoading(false);
+        setError(false);
+        pageNumber += 1;
+        console.log(pageNumber);
+      })
+      .catch(handleErrors);
+  };
 
-  const lastElementRef = useCallback(
-    (node) => {
-      if (loading) return;
-      if (observe.current) observe.current.disconnect();
-      observe.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && inComplete) {
-          console.log("visible");
-          setPageNumber((prev) => prev + 1);
-          console.log(pageNumber);
-        }
-      });
-      if (node) observe.current.observe(node);
-    },
-    [loading, inComplete, pageNumber]
-  );
+  const handleScroll = (e) => {
+    if (
+      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
+      e.target.documentElement.scrollHeight
+    ) {
+      fetchData();
+    }
+  };
+  useEffect(() => {
+    fetchData();
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  const handleErrors = (err) => {
+    if (err.response || err.request) {
+      setErrorMessage(
+        "oops! error in connection :( try reloading page or check your internet connection"
+      );
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div>
       <div className="main">
-        {val.map((item, index) => {
-          if (item.length === index + 1) {
-            return (
-              <div ref={lastElementRef} className="sub-main-div" key={item.id}>
-                <div className="img-div">
-                  <img src={item.owner.avatar_url} alt="" />
-                </div>
-                <div className="name-desc">
-                  <h3>{item.name}</h3>
-                  <p className="desc">{item.description}</p>
-                  <div className="counts">
-                    <p className="star">
-                      Stars: <span>{item.stargazers_count}</span>
-                    </p>
-                    <p className="issues">
-                      Issues: <span>{item.open_issues_count}</span>
-                    </p>
-                    <p>
-                      pushed at <span>{moment(item.pushed_at).fromNow()}</span>{" "}
-                      by <span>{item.owner.login}</span>
-                    </p>
-                  </div>
-                </div>
+        {val.map((item, index) => (
+          <div className="sub-main-div" key={index}>
+            <div className="img-div">
+              <img src={item.owner.avatar_url} alt="" />
+            </div>
+            <div className="name-desc">
+              <h3>{item.name}</h3>
+              <p className="desc">{item.description}</p>
+              <div className="counts">
+                <p className="star">
+                  Stars: <span>{item.stargazers_count}</span>
+                </p>
+                <p className="issues">
+                  Issues: <span>{item.open_issues_count}</span>
+                </p>
+                <p className="time">
+                  pushed at <span>{moment(item.pushed_at).fromNow()}</span> by{" "}
+                  <span>{item.owner.login}</span>
+                </p>
               </div>
-            );
-          } else {
-            return (
-              <div className="sub-main-div" key={item.id}>
-                <div className="img-div">
-                  <img src={item.owner.avatar_url} alt="" />
-                </div>
-                <div className="name-desc">
-                  <h3>{item.name}</h3>
-                  <p className="desc">{item.description}</p>
-                  <div className="counts">
-                    <p className="star">
-                      Stars: <span>{item.stargazers_count}</span>
-                    </p>
-                    <p className="issues">
-                      Issues: <span>{item.open_issues_count}</span>
-                    </p>
-                    <p>
-                      pushed at <span>{moment(item.pushed_at).fromNow()}</span>{" "}
-                      by <span>{item.owner.login}</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-        })}
+            </div>
+          </div>
+        ))}
       </div>
       {loading && <div className="loading"></div>}
+      {error && <div className="error">{errorMessage}</div>}
     </div>
   );
 }
