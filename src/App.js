4c476a9f9b1ed1eb8a920a7789e2b58215ce01bb
Sylar <user@Sylar-MacBook-Pro.local>
2022-07-21 16:36:47 +0100

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Display from "./components/Display";
-//import Dis from "./components/Dis";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
-      {/* <Dis /> */}
       <Display />
     </div>
   );
